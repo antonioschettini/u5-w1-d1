0858abf7fd774e1b8dfaa1f4a28e61dbb6812f1c
antonio.schettini <antonio.schettini93@gmail.com>
2026-06-29 14:22:12 +0200

    commit

--- src/main/java/antonioschettini/u5_w1_d1/entities/Menu.java
+++ src/main/java/antonioschettini/u5_w1_d1/entities/Menu.java
@@ -14,7 +14,6 @@ public class Menu {
     private List<Topping> toppings;
     private List<Drink> drinks;
 
-
     // Creo un metodo per stampare il menù nel sout
     public void stampaMenu() {
         System.out.println("-----------------------------------------");
@@ -22,23 +21,26 @@ public class Menu {
         System.out.println("-----------------------------------------");
 
         System.out.println("Lista Pizze");
-        for (Pizza pizza : pizzas) {
-            System.out.println(pizza.getNome() + " Calorie: " + pizza.getCalorie() + " Prezzo " + pizza.getPrezzo() + " €");
-        }
+        pizzas.forEach(pizza -> System.out.println(pizza.getNome() + " Calorie: " + pizza.getCalorie() + " Prezzo " + pizza.getPrezzo() + " €"));
+//        for (Pizza pizza : pizzas) {
+//            System.out.println(pizza.getNome() + " Calorie: " + pizza.getCalorie() + " Prezzo " + pizza.getPrezzo() + " €");
+//        }
 
         System.out.println("-----------------------------------------");
 
         System.out.println("Toppings");
-        for (Topping topping : toppings) {
-            System.out.println(topping.getNome() + " Calorie: " + topping.getCalorie() + " Prezzo " + topping.getPrezzo() + " €");
-        }
+        toppings.forEach(topping -> System.out.println(topping.getNome() + " Calorie: " + topping.getCalorie() + " Prezzo " + topping.getPrezzo() + " €"));
+//        for (Topping topping : toppings) {
+//            System.out.println(topping.getNome() + " Calorie: " + topping.getCalorie() + " Prezzo " + topping.getPrezzo() + " €");
+//        }
 
         System.out.println("-----------------------------------------");
 
         System.out.println("Drinks");
-        for (Drink drink : drinks) {
-            System.out.println(drink.getNome() + " Calorie: " + drink.getCalorie() + " Prezzo " + drink.getPrezzo() + " €");
-        }
+        drinks.forEach(drink -> System.out.println(drink.getNome() + " Calorie: " + drink.getCalorie() + " Prezzo " + drink.getPrezzo() + " €"));
+//        for (Drink drink : drinks) {
+//            System.out.println(drink.getNome() + " Calorie: " + drink.getCalorie() + " Prezzo " + drink.getPrezzo() + " €");
+//        }
 
         System.out.println("-----------------------------------------");
     }
